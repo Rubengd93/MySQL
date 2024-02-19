d094a0a523d3cc6b815877261f52acd31e373d01
Rubengd93 <rubengd93@gmail.com>
2024-02-19 21:35:50 +0100

    Retos dia1 mysql

---
+++ Tema_1/insertStudent.sql
@@ -0,0 +1,11 @@
+INSERT INTO students (first_name, last_name, group_id) VALUES 
+('Laura', 'García', 1),
+('Carlos', 'Fernández', 2),
+('Ana', 'Martínez', 3),
+('David', 'González', 4),
+('Sofía', 'López', 5),
+('Daniel', 'Gómez', 1),
+('María', 'Ruiz', 2),
+('Jorge', 'Moreno', 3),
+('Elena', 'Jiménez', 4),
+('Luis', 'Díaz', 5);
---
+++ Tema_1/insertStudents.sql
@@ -0,0 +1,11 @@
+INSERT INTO students (first_name, last_name) VALUES 
+('Laura', 'García'),
+('Carlos', 'Fernández'),
+('Ana', 'Martínez'),
+('David', 'González'),
+('Sofía', 'López'),
+('Daniel', 'Gómez'),
+('María', 'Ruiz'),
+('Jorge', 'Moreno'),
+('Elena', 'Jiménez'),
+('Luis', 'Díaz');
